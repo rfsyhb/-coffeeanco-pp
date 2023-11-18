6887999f2f9b9464e39704dbc81d5f6ac13bbf5c
rfsyhb <rafisyihab3@gmail.com>
2023-11-18 19:35:57 +0700

    adding navbar

---
+++ includes/navbar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <a href="#" class="logo">
+            <i class="ri-shopping-bag-2-line"></i><span>CoffeeanCo</span>
+        </a>
+        <ul class="navbar">
+            <li>
+                <a href="#" class="btn">Home</a>
+            </li>
+            <li>
+                <a href="#" class="btn">Product</a>
+            </li>
+            <li>
+                <a href="#" class="btn">Learn</a>
+            </li>
+            <li>
+                <a href="#" class="btn">Account</a>
+            </li>
+        </ul>
+
+        <div class="main">
+            <a href="#" class="cart">
+                <i class="ri-shopping-cart-line"></i>
+            </a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+    </header>
+    <!-- js link -->
+    <script type="text/javascript" src="../assets/js/navbarscript.js"></script>
+</body>
+
+</html>
