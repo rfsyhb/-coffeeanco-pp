57bb4599ddcc7c33577ce9eee28d845885039318
rfsyhb <rafisyihab3@gmail.com>
2023-11-23 05:16:32 +0700

    adding status pemesanan

--- admin/index.php
+++ admin/index.php
@@ -6,6 +6,37 @@ if ($_SESSION['status'] != "admin") {
 }
 
 include "../includes/config.php";
+
+// Cek jika action dan order_id diterima melalui GET request
+if (isset($_GET['action']) && isset($_GET['order_id'])) {
+    $order_id = $_GET['order_id'];
+    $action = $_GET['action'];
+
+    switch ($action) {
+        case 'fail':
+            $new_status = 'digagalkan';
+            break;
+        case 'process':
+            $new_status = 'akan diproses';
+            break;
+        case 'delay':
+            $new_status = 'delayed';
+            break;
+        case 'complete':
+            $new_status = 'diproses';
+            break;
+        default:
+            die('Invalid action.');
+    }
+
+    $query = "UPDATE orders SET order_status = '$new_status' WHERE order_id = '$order_id'";
+    mysqli_query($connect, $query);
+
+    // Redirect back to the page to avoid re-executing the URL on refresh
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +54,7 @@ include "../includes/config.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
 
     <link href="../assets/css/admin.css" rel="stylesheet" />
@@ -57,7 +88,7 @@ include "../includes/config.php";
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
                     <div class="nav">
-                        
+
                         <a class="nav-link mt-3" href="index.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
@@ -96,16 +127,71 @@ include "../includes/config.php";
         </div>
         <div id="layoutSidenav_content">
             <main>
-
                 <!-- Page Content -->
                 <div id="page-content-wrapper">
                     <div class="container-fluid px-4">
                         <?php
                         include 'container_fluid.php';
                         ?>
+                        <div class="row">
+                            <div class="col-md-8 offset-md-2 mt-5">
+                            <h3 class="fs-3 mb-3 ">Status Pemesanan</h3>
+                                <table class="table bg-white rounded shadow-sm table-hover pad">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Nama Customer</th>
+                                            <th scope="col">Order ID</th>
+                                            <th scope="col" width="110">Order Date</th>
+                                            <th scope="col" width="120">Order Status</th>
+                                            <th scope="col" width="143">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $datas = mysqli_query($connect, "SELECT pengunjung.cust_name, orders.order_id, orders.order_date, orders.order_status FROM orders INNER JOIN pengunjung ON orders.cust_id = pengunjung.cust_id");
+                                        while ($data = mysqli_fetch_array($datas)) {
+                                            ?>
+                                            <tr>
+                                                <td>
+                                                    <?php echo $data['cust_name']; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $data['order_id']; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $data['order_date']; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $data['order_status']; ?>
+                                                </td>
+                                                <td>
+                                                    <a href="?action=fail&order_id=<?php echo $data['order_id']; ?>"
+                                                        class="btn-sm btn-primary">
+                                                        <span class="fa-regular fa-calendar-xmark">
+                                                    </a>
+                                                    <a href="?action=process&order_id=<?php echo $data['order_id']; ?>"
+                                                        class="btn-sm btn-primary">
+                                                        <span class="fa-regular fa-calendar-plus">
+                                                    </a>
+                                                    <a href="?action=delay&order_id=<?php echo $data['order_id']; ?>"
+                                                        class="btn-sm btn-primary">
+                                                        <span class="fa-regular fa-calendar-minus">
+                                                    </a>
+                                                    <a href="?action=complete&order_id=<?php echo $data['order_id']; ?>"
+                                                        class="btn-sm btn-danger">
+                                                        <span class="fa-regular fa-calendar-check">
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </main>
             <footer class="py-4 bg-light mt-1">
                 <div class="container-fluid px-4">
